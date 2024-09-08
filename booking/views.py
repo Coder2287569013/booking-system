b80ed082ede8cb9a631e87106a5fd860e61ad94c
Coder2287569013 <yarovenkovova2008@gmail.com>
2024-09-08 09:13:39 -0230

    Add new view

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room, Booking
+from django.http import HttpResponse
 
 # Create your views here.
 def room_list(request):
@@ -10,3 +11,25 @@ def room_list(request):
     }
 
     return render(request, "booking/room_list.html", context)
+
+def book_room(request):
+    if request.method == "POST":
+        room_number = request.POST.get("room-number")
+        start_time = request.POST.get("start-time")
+        end_time = request.POST.get("end-time")
+
+        try:
+            room = Room.objects.get(number=room_number)
+        except ValueError:
+            return HttpResponse("Wrong room number!", status_code=400)
+        
+        booking = Booking.objects.create(
+            user=request.user,
+            room=room,
+            start_time=start_time,
+            end_time=end_time
+        )
+
+        return redirect("booking-detail", pk=booking.id)
+    else:
+        return render(request, template_name="booking/booking_form.html")
